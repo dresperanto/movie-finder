342e463566dd3e4b0e21dd20c56ce1b7fb933ab7
Will Rainbow <will@muzemaster.com>
2019-02-25 16:08:45 -0500

    Change Home component from fetch to async

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
-import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
-import HeroImage from '../elements/HeroImage/HeroImage';
-import SearchBar from '../elements/SearchBar/SearchBar';
-import FourColGrid from '../elements/FourColGrid/FourColGrid';
-import MovieThumb from '../elements/MovieThumb/MovieThumb';
-import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
-import Spinner from '../elements/Spinner/Spinner';
-import './Home.css';
+import React, { Component } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../../config";
+import HeroImage from "../elements/HeroImage/HeroImage";
+import SearchBar from "../elements/SearchBar/SearchBar";
+import FourColGrid from "../elements/FourColGrid/FourColGrid";
+import MovieThumb from "../elements/MovieThumb/MovieThumb";
+import LoadMoreBtn from "../elements/LoadMoreBtn/LoadMoreBtn";
+import Spinner from "../elements/Spinner/Spinner";
+import "./Home.css";
 
 class Home extends Component {
   state = {
@@ -15,8 +21,8 @@ class Home extends Component {
     loading: false,
     currentPage: 0,
     totalPages: 0,
-    searchTerm: ''
-  }
+    searchTerm: ""
+  };
 
   componentDidMount() {
     this.setState({ loading: true });
@@ -24,85 +30,128 @@ class Home extends Component {
     this.fetchItems(endpoint);
   }
 
-  searchItems = (searchTerm) => {
+  searchItems = searchTerm => {
     console.log(searchTerm);
-    let endpoint = '';
+    let endpoint = "";
     this.setState({
       movies: [],
       loading: true,
       searchTerm
-    })
+    });
 
-    if (searchTerm === '') {
+    if (searchTerm === "") {
       endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
     } else {
       endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
     }
     this.fetchItems(endpoint);
-
-  }
+  };
 
   loadMoreItems = () => {
-    let endpoint = '';
+    let endpoint = "";
     this.setState({ loading: true });
 
-    if (this.state.searchTerm === '') {
-      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    if (this.state.searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
     } else {
-      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${
+        this.state.searchTerm
+      }&page=${this.state.currentPage + 1}`;
     }
     this.fetchItems(endpoint);
-  }
+  };
 
-  fetchItems = (endpoint) => {
-    fetch(endpoint)
-      .then(result => result.json())
-      .then(result => {
-        this.setState({
-          movies: [...this.state.movies, ...result.results],
-          heroImage: this.state.heroImage || result.results[0],
+  fetchItems = async endpoint => {
+    const { movies, heroImage, searchTerm } = this.state;
+    const result = await (await fetch(endpoint)).json();
+    try {
+      this.setState(
+        {
+          movies: [...movies, ...result.results],
+          heroImage: heroImage || result.results[0],
           loading: false,
           currentPage: result.page,
           totalPages: result.total_pages
-        })
-      })
-      .catch(error => console.error('Error:', error))
-  }
+        },
+        () => {
+          if (searchTerm === "") {
+            sessionStorage.setItem("Homestate", JSON.stringify(this.state));
+          }
+        }
+      );
+    } catch (e) {
+      console.log("There was and error", e);
+    }
+  };
+
+  // fetchItems = endpoint => {
+  //   const { movies, heroImage, searchTerm } = this.state;
+  //   fetch(endpoint)
+  //     .then(result => result.json())
+  //     .then(result => {
+  //       this.setState(
+  //         {
+  //           movies: [...movies, ...result.results],
+  //           heroImage: heroImage || result.results[0],
+  //           loading: false,
+  //           currentPage: result.page,
+  //           totalPages: result.total_pages
+  //         },
+  //         () => {
+  //           if (searchTerm === "") {
+  //             sessionStorage.setItem("Homestate", JSON.stringify(this.state));
+  //           }
+  //         }
+  //       );
+  //     })
+  //     .catch(error => console.error("Error:", error));
+  // };
 
   render() {
     return (
       <div className="rmdb-home">
-        {this.state.heroImage ?
+        {this.state.heroImage ? (
           <div>
             <HeroImage
-              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${
+                this.state.heroImage.backdrop_path
+              }`}
               title={this.state.heroImage.original_title}
               text={this.state.heroImage.overview}
             />
             <SearchBar callback={this.searchItems} />
-          </div> : null}
+          </div>
+        ) : null}
         <div className="rmdb-home-grid">
           <FourColGrid
-            header={this.state.searchTerm ? 'Search Result' : 'Popular Movies'}
+            header={this.state.searchTerm ? "Search Result" : "Popular Movies"}
             loading={this.state.loading}
           >
             {this.state.movies.map((element, i) => {
-              return <MovieThumb
-                key={i}
-                clickable={true}
-                image={element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : './images/no_image.jpg'}
-                movieId={element.id}
-                movieName={element.original_title}
-              />
+              return (
+                <MovieThumb
+                  key={i}
+                  clickable={true}
+                  image={
+                    element.poster_path
+                      ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`
+                      : "./images/no_image.jpg"
+                  }
+                  movieId={element.id}
+                  movieName={element.original_title}
+                />
+              );
             })}
           </FourColGrid>
           {this.state.loading ? <Spinner /> : null}
-          {(this.state.currentPage <= this.state.totalPages && !this.state.loading) ?
+          {this.state.currentPage <= this.state.totalPages &&
+          !this.state.loading ? (
             <LoadMoreBtn text="Load More" onClick={this.loadMoreItems} />
-            : null}
+          ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
